485084185231dd7459943ebd25c554025921d9ce
Ayush Jain <ayushjain15515@gmail.com>
2026-09-19 21:34:14 +0530

    Increase image upload limit

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,9 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage: storage,
+  limits: {
+    fileSize: 20 * 1024 * 1024, // 20 MB
+  },
 });
 
 // =====================================
